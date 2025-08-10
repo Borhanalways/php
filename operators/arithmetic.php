fc25b6254ce665da2a5667f8117f15cbaef09c17
Borhan <borhanuddin0600@gmail.com>
2025-08-10 10:37:11 +0600

    second commit

---
+++ operators/arithmetic.php
@@ -0,0 +1,16 @@
+   //Arithmetic Operators
+        $a = 15;
+        $b = 4;
+        $addition = $a + $b;
+        $subtraction = $a - $b;
+        $multiplication = $a * $b;
+        $division = $a / $b;
+        $modulus = $a % $b;
+        $exponentiation = $a ** $b;
+
+        echo "$addition<br>";
+        echo "$subtraction<br>";
+        echo "$multiplication<br>";
+        echo "$division<br>";
+        echo "$modulus<br>";
+        echo "$exponentiation<br>";
